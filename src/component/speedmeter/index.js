d036d9c0b382dc4a5f2f785ee445253caaebd2a3
Wesley Moura <mouraiv@gmail.com>
2021-10-27 13:46:26 -0300

    disable rotate

--- src/component/speedmeter/index.js
+++ src/component/speedmeter/index.js
@@ -4,7 +4,8 @@ import {
   Text,
   Animated,
   Easing,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView
 } from 'react-native';
 
 import EStylesheet from 'react-native-extended-stylesheet';
@@ -52,6 +53,7 @@ function SpeedMeter ({route, navigation}) {
 
   return (
 
+<ScrollView>
   <SafeAreaView style={styles.safeAreaContainer}>
 
     <View style={styles.container}>
@@ -80,10 +82,10 @@ function SpeedMeter ({route, navigation}) {
         <Path fill="rgb(255,215,0)" d="M 152.973 179.391 L 193.954 222.223 C 211.162 204.535 231.025 204.453 237.213 204.436 L 237.273 144.387 C 207.749 143.577 176.734 154.694 152.973 179.391 Z"/>
         <Path fill="rgb(255, 0, 0)" d="M 280.534 180.675 C 279.931 208.429 293.036 244.152 314.636 265.298 L 357.059 223.807 C 342.445 209.706 339.268 191.567 339.311 181.475 L 280.534 180.675 Z" transform="matrix(-1, 0, 0, -1, 637.573059, 445.972992)"/>
         <Path fill="rgb(255, 140, 0)" d="M 238.384 185.723 L 280.716 144.242 C 294.047 157.486 310.636 160.584 322.045 161.064 L 321.912 221.306 C 289.556 219.03 263.459 209.846 238.384 185.723 Z" transform="matrix(-1, 0, 0, -1, 560.429016, 365.548004)"/>
-        <SvgText fill="rgb(51, 51, 51)" transform="matrix(0.341878, -0.939744, 0.939744, 0.341878, -64.588371, 288.816345)" x="116.293" y="170.115">BAIXO</SvgText>
-        <SvgText fill="rgb(51, 51, 51)" transform="matrix(0.921104, -0.389316, 0.389316, 0.921104, 5.641595, 60.816177)" x="118.293" y="170.115">MÉDIO</SvgText>
-        <SvgText fill="rgb(51, 51, 51)" transform="matrix(0.923597, 0.383365, -0.383365, 0.923597, 225.360291, -41.276188)" x="116.293" y="170.115">ALTO</SvgText>
-        <SvgText fill="rgb(51, 51, 51)" transform="matrix(0.356224, 0.934401, -0.934401, 0.356224, 447.123962, 38.390083)" x="117.293" y="170.115">CRÍTICO</SvgText>
+        <SvgText fill="rgb(0, 0, 0)" fontWeight='bold' fontFamily='sans-serif-light' transform="matrix(0.341878, -0.939744, 0.939744, 0.341878, -64.588371, 288.816345)" x="115.293" y="172.115">BAIXO</SvgText>
+        <SvgText fill="rgb(0, 0, 0)" fontWeight='bold' fontFamily='sans-serif-light' transform="matrix(0.921104, -0.389316, 0.389316, 0.921104, 5.641595, 60.816177)" x="118.293" y="172.115">MÉDIO</SvgText>
+        <SvgText fill="rgb(0, 0, 0)" fontWeight='bold' fontFamily='sans-serif-light' transform="matrix(0.923597, 0.383365, -0.383365, 0.923597, 225.360291, -41.276188)" x="115.293" y="172.115">ALTO</SvgText>
+        <SvgText fill="rgb(0, 0, 0)" fontWeight='bold' fontFamily='sans-serif-light' transform="matrix(0.356224, 0.934401, -0.934401, 0.356224, 447.123962, 38.390083)" x="112.293" y="172.115">CRÍTICO</SvgText>
       </Svg>
 
       </View>
@@ -131,6 +133,7 @@ function SpeedMeter ({route, navigation}) {
   
     </View>
   </SafeAreaView>
+  </ScrollView>  
   )
 };
 
@@ -151,6 +154,7 @@ const styles = EStylesheet.create({
     width: '25.9rem',
     height: '25.9rem',
     marginTop: '3rem',
+    alignSelf: 'center',
     position: 'absolute',
     zIndex:2
   },
@@ -163,7 +167,7 @@ const styles = EStylesheet.create({
     zIndex:3
   },
   speedResult:{
-    fontSize: 48,
+    fontSize: '3rem',
     textAlign:'center',
     marginTop: '9.5rem',
     alignItems: 'center',
@@ -171,7 +175,7 @@ const styles = EStylesheet.create({
   },
   speedResultList:{
     padding: '2rem',
-    height: '24.7rem',
+    height: '24.6rem',
     zIndex:5,
   },
   speedResultText:{
